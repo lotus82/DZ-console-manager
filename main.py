f2de0669048f4b5fb775335573bc840a4edd99bc
Dmitriy Suhov <lotus.it@mail.ru>
2022-04-20 23:52:30 +0400

    Add other functions in main.py and add victorina.py and bank.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 import shutil
+import getpass
+import socket
+from uuid import getnode as get_mac
+import platform
+import victorina.victorina as vic
+import bank.bank as b
 
 IS_ACTIVE = True
+OWNER = "Дмитрий Сухов"
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 menu_dict = {
     '1':'Создать папку',
     '2':'Удалить (файл/папку)',
@@ -32,7 +40,7 @@ def main_menu():
         count+=1
     print("=" * 84)
 
-# Создать папку
+# 1 - Создать папку
 def create_folder():
     try:
         dir_name = input("Введите имя папки: ")
@@ -42,7 +50,7 @@ def create_folder():
     else:
         print("-" * 84 + "\nСоздана директория: %s " % dir_name + "\n" + "-"*84)
 
-# Удалить (файл/папку)
+# 2 - Удалить (файл/папку)
 def del_folder():
     path = input("Введите имя папки или файла: ")
     if os.path.exists(path):
@@ -64,43 +72,87 @@ def del_folder():
     else:
         print("-" * 84 + "\nТакого файла или папки здесь нет: %s " % path + "\n" + "-" * 84)
 
-# Копировать (файл/папку)
+# 3 - Копировать (файл/папку)
 def copy_file():
-    pass
+    inp = input("Введите что копировать: ")
+    outp = input("Введите куда копировать: ")
+    if os.path.exists(inp):
+        if os.path.isfile(inp):
+            try:
+                shutil.copyfile(inp, outp)
+                print("-" * 84 + "\nСкопирован файл: %s " % inp + " в: %s " % outp + "\n" + "-" * 84)
+            except Exception as err:
+                print("Ошибка копирования файла: %s " % inp + " в: %s " % outp, ":", err)
+        else:
+            try:
+                shutil.copytree(inp, outp)
+                print("-" * 84 + "\nСкопирована папка: %s " % inp + " в: %s " % outp + "\n" + "-" * 84)
+            except Exception as err:
+                print("Ошибка копирования папки: %s " % inp + " в %s " % outp, ":", err)
+    else:
+        print("-" * 84 + "\nТакого файла или папки здесь нет: %s " % inp + "\n" + "-" * 84)
 
-# Просмотр содержимого рабочей директории
-def list_path():
-    pass
+# 4,5,6 - Просмотр содержимого рабочей директории
+def list_path(param="all"):
+    global CUR_DIR
+    try:
+        content = os.listdir(CUR_DIR)
+        print("-" * 84)
+        for item in content:
+            if param == "files":
+                if os.path.isfile(os.path.join(CUR_DIR,item)):
+                    print(item)
+            if param == "folders":
+                if not os.path.isfile(os.path.join(CUR_DIR,item)):
+                    print(item)
+            if param == "all":
+                print(item)
+        print("-" * 84)
+    except Exception as err:
+        print("Ошибка просмотра содержимого рабочей директории: %s " % CUR_DIR, ":", err)
 
-# Посмотреть только папки
-def list_folders():
-    pass
-
-# Посмотреть только файлы
-def list_files():
-    pass
-
-# Просмотр информации об ОС
+# 7 - Просмотр информации об ОС
 def info_os():
-    pass
+    try:
+        info_dict = {
+            "Имя пользователя" : getpass.getuser(),
+            "IP-адрес системы" : socket.gethostbyname(socket.getfqdn()),
+            "MAC адрес" : get_mac(),
+            "Название операционной системы" : platform.uname()
+        }
+        print("-" * 84 )
+        for k,v in info_dict.items():
+            print(k + " --> ", v)
+        print("-" * 84)
+    except Exception as err:
+        print("Ошибка получения информации: ", err)
 
-# Создатель программы
+# 8 - Создатель программы
 def info_owner():
-    pass
+    global OWNER
+    print("-" * 84)
+    print("Создатель программы: ", OWNER)
+    print("-" * 84)
 
-# Играть в викторину
+# 9 - Играть в викторину
 def game():
-    pass
+    vic.start_game()
 
-# Мой банковский счет
+# 10 - Мой банковский счет
 def bank():
-    pass
+    b.start_bank()
 
-# Смена рабочей директории
+# 11 - Смена рабочей директории
 def change_path():
-    pass
+    global CUR_DIR
+    new_path = input("Сменить рабочую директорию. Введите новый путь: ")
+    try:
+        os.chdir(new_path)
+        CUR_DIR = new_path
+    except Exception as err:
+        print("Ошибка смены рабочей директории на : %s" % new_path, ":", err)
 
-# Выход
+# 12 - Выход
 def close_app():
     global IS_ACTIVE
     IS_ACTIVE = False
@@ -120,9 +172,9 @@ def run_app():
         elif choice == '4':
             list_path()
         elif choice == '5':
-            list_folders()
+            list_path("folders")
         elif choice == '6':
-            list_files()
+            list_path("files")
         elif choice == '7':
             info_os()
         elif choice == '8':
