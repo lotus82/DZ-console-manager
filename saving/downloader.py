6788db0bbd546a3b1fac875fd5ff77287d31c569
lotus82 <lotus.it@mail.ru>
2022-05-24 10:17:12 +0400

    saving

---
+++ saving/downloader.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+def read_csv(path):
+    if os.path.exists(path):
+        history = []
+        with open(path, encoding='utf-8') as r_file:
+            file_reader = csv.reader(r_file, delimiter=",")
+            count = 0
+            for row in file_reader:
+                history.append({row[0]: row[1]})
+    else:
+        print('[ERROR]: отсутствует файл ', path)
+    return history
+
+def read_txt(path):
+    score = 0
+    if os.path.exists(path):
+
+        with open(path, encoding='utf-8') as r_file:
+            score = int(r_file.readline())
+    else:
+        print('[ERROR]: отсутствует файл ', path)
+    return  score
---
+++ saving/uploader.py
@@ -0,0 +1,41 @@
+import csv
+import os.path
+import pickle
+
+
+def save_history(path, history_dict):
+    try:
+        with open(path, mode="w", encoding='utf-8') as w_file:
+            file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+            for item in history_dict:
+                for k, v in item.items():
+                    file_writer.writerow([k, v])
+    except Exception as err:
+        print('[ERROR]: ошибка сохранения истории покупок: ', err)
+
+def save_score(path, score):
+    try:
+        with open(path, mode="w", encoding='utf-8') as w_file:
+            w_file.write(str(score))
+    except Exception as err:
+        print('[ERROR]: ошибка сохранения счета: ', err)
+
+
+def save_listdir(path, lst, cur_dir):
+    try:
+        folders = []
+        files = []
+        for item in lst:
+            if os.path.isfile(os.path.join(cur_dir, item)):
+                files.append(item)
+            else:
+                folders.append(item)
+        with open(path, mode="w", encoding='utf-8') as w_file:
+            w_file.write('folders: ')
+            w_file.write(', '.join(folders))
+            w_file.write('\nfiles: ')
+            w_file.write(', '.join(files))
+        return True
+    except Exception as err:
+        print('[ERROR]: ошибка сохранения содержимого рабочей директории: ', err)
+        return False
