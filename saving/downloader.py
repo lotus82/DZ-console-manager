859da61fa6cbb425401a20cbce7151e112b963fa
lotus82 <lotus.it@mail.ru>
2022-06-04 07:38:38 +0400

    add generators add decorators add except

--- saving/downloader.py
+++ saving/downloader.py
@@ -1,23 +1,34 @@
 import csv
 import os
 
+# чтение истории покупок
+# использование генератора списков
+# обработка исключений
 def read_csv(path):
-    if os.path.exists(path):
+    try:
         history = []
-        with open(path, encoding='utf-8') as r_file:
-            file_reader = csv.reader(r_file, delimiter=",")
-            for row in file_reader:
-                history.append({row[0]: row[1]})
-    else:
-        print('[ERROR]: отсутствует файл ', path)
-    return history
+        if os.path.exists(path):
+            with open(path, encoding='utf-8') as r_file:
+                file_reader = csv.reader(r_file, delimiter=";")
+                history = [{row[0]: row[1]} for row in file_reader]
+        else:
+            print('[ERROR]: отсутствует файл ', path)
+    except Exception as err:
+        print('[ERROR]: ошибка чтения истории покупок: ', err)
+    finally:
+        return history
 
+# чтение баланса
+# обработка исключений
 def read_txt(path):
-    score = 0
-    if os.path.exists(path):
-
-        with open(path, encoding='utf-8') as r_file:
-            score = int(r_file.readline())
-    else:
-        print('[ERROR]: отсутствует файл ', path)
-    return  score
+    try:
+        score = 0
+        if os.path.exists(path):
+            with open(path, encoding='utf-8') as r_file:
+                score = int(r_file.readline())
+        else:
+            print('[ERROR]: отсутствует файл ', path)
+    except Exception as err:
+        print('[ERROR]: ошибка чтения баланса: ', err)
+    finally:
+        return  score
